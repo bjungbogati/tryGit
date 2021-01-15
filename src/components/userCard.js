f82e15bd4c8fea1c21a14bab064f69daba964f03
Binod Jung Bogati <8600091+bjungbogati@users.noreply.github.com>
2021-01-15 16:43:56 +0545

    Update userCard.js

--- src/components/userCard.js
+++ src/components/userCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Card = ({fullname, username}) => (
-    <a href={"https://github.com/"+username} className="profile">
+    <a rel="nofollow noopener" href={"https://github.com/"+username} className="profile">
         <img src={"https://avatars1.githubusercontent.com/"+username+"?size=200"}/>
         <p>{fullname}</p>
     </a>
